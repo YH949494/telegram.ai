2e6370280d15e893e628186d8caab8cd5ff5b17b
YH949494 <teohyaohui@gmail.com>
2026-04-21 11:40:20 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from functools import lru_cache
 from typing import Set, Optional
 
-from pydantic import BaseSettings, Field
-
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     """
